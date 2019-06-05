2822a17a37180e44e35d239bd0559e831bda8254
siman <siman@ddsus.net>
2019-06-05 09:11:31 -0500

    dashboard

---
+++ src/components/views/Manage_Orders.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Manage_Orders() {
+    return (
+        <div>
+         <h1>Manage Orderssssssssssssssssssssssssssss</h1>  
+         <h1>Manage Orderssssssssssssssssssssssssssss</h1>  
+         <h1>Manage Orderssssssssssssssssssssssssssss</h1>        
+        </div>
+    )
+}
+
+export default Manage_Orders
+        
